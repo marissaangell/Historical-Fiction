d3bdae22089ca73876af4573fe270a4ab860d5c2
Marissa Angell <maa.angell@gmail.com>
2019-12-07 15:48:55 -0700

    update styles/scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,9 @@
+$("#credits-button").click(function(){ 
+  $("#menu").hide(); 
+  $("#credits").show(); 
+})
 
+$("#back-button").click(function(){ 
+  $("#credits").hide(); 
+  $("#menu").show(); 
+})
