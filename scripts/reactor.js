4e740bffb29e493f7f9e69e8a21b4bb3754e6fb1
Marissa Angell <maa.angell@gmail.com>
2019-12-10 12:58:25 -0700

    Finish reactor minigame

--- scripts/reactor.js
+++ scripts/reactor.js
@@ -1,16 +1,25 @@
 var count = 1;
+var targetPower;
+var xenonBool;
+var numControlRods = 0;
 
 $(document).keypress(function(event){ 
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13'){
-  	count++;
+  	if (count != 14){
+  		count++;
 
-  	var currID = "#typewriter-".concat(count.toString());
-  	var curr = $(currID);
+	  	var currID = "#typewriter-".concat(count.toString());
+	  	var curr = $(currID);
 
-  	
-  	curr.removeClass("typewriter-before");
-  	curr.addClass("typewriter");
+	  	
+	  	curr.removeClass("typewriter-before");
+	  	curr.addClass("typewriter");
+  	}
+
+  	if (count == 20){
+  		validate_answers();
+  	}
   }
 })
 
@@ -22,6 +31,7 @@ $('#power-input').keypress(function(event){
     	var input = $("#power-input").val();
         if (power_validate(input)){
         	$("#power-output").val(input);
+        	targetPower = input;
         }
     }
 
@@ -48,8 +58,10 @@ $('#xenon-input').keypress(function(event){
         if (xenon_validate(input)){
         	if (input.toLowerCase() == "y"){
         		$("#xenon-output").val("Starting xenon injection.");
+        		xenonBool = true;
         	} else if (input.toLowerCase() == "n"){
         		$("#xenon-output").val("Xenon injection aborted.");
+        		xenonBool = false;
         	}
         }
     }
@@ -64,5 +76,102 @@ function xenon_validate(input){
 	} else {
 		$("#xenon-input").attr("readonly", "readonly");
 		return true;
+	}
+}
+
+// CONTROL BOOL
+
+$('#control-bool-input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+    	var input = $("#control-bool-input").val();
+        if (control_bool_validate(input)){
+        	if (input.toLowerCase() == "y"){
+        		event.stopPropagation();
+        		
+        		//go to to insert rods section
+        		count = 16;
+
+        		var currID = "#typewriter-".concat(count.toString());
+			  	var curr = $(currID);
+
+			  	curr.removeClass("typewriter-before");
+			  	curr.addClass("typewriter");
+        	} else if (input.toLowerCase() == "n"){
+        		event.stopPropagation();
+        		
+        		//skip insert rods section
+        		count = 18;
+
+        		var currID = "#typewriter-".concat(count.toString());
+			  	var curr = $(currID);
+
+			  	curr.removeClass("typewriter-before");
+			  	curr.addClass("typewriter");
+        	}
+        }
+    }
+
+});
+
+function control_bool_validate(input){
+	if (!(input.toLowerCase()).match("y") && !(input.toLowerCase()).match("n")){
+		event.stopPropagation();
+		alert("Invalid input. Try again.");
+		return false;
+	} else {
+		$("#control-bool-input").attr("readonly", "readonly");
+		return true;
+	}
+}
+
+// CONTROL ROD NUM
+
+$('#control-num-input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+    	var input = $("#control-num-input").val();
+        if (control_num_validate(input)){
+        	$("#control-num-output").val(input);
+        	numControlRods = input;
+        }
+    }
+
+});
+
+function control_num_validate(input){
+	if (input.match(/[a-zA-Z]$/) || input == "" || input > 211 || input < 0){
+		event.stopPropagation();
+		alert("Invalid input. Try again.");
+		return false;
+	} else {
+		$("#control-num-input").attr("readonly", "readonly");
+		return true;
+	}
+
+}
+
+// ANSWER VALIDATION
+
+function validate_answers(){
+	var targetPowerCorrect = (targetPower > 700 && targetPower < 5000);
+	var xenonCorrect = !xenonBool;
+	var numControlRodsCorrect = (numControlRods > 25 && numControlRods < 101);
+
+	if (targetPowerCorrect && xenonCorrect && numControlRodsCorrect){
+		$("#result").val("Reactor output at normal levels.");
+		$("#result").attr("style", "color: green; width: 600px;");
+
+		setTimeout(function(){ 
+        	$("#success").removeClass("typewriter-before");
+    	}, 3000);  
+
+	} else {
+		$("#result").val("WARNING: Reactor output rising. Meltdown imminent.");
+		$("#result").attr("style", "color: red; width: 825px;");
+		
+		setTimeout(function(){ 
+        	$("#failure").removeClass("typewriter-before");
+    	}, 3000);  
 	}
 }
