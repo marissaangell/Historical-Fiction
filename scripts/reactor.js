922300e621303cb1e3373be559180d896b7acdee
Marissa Angell <maa.angell@gmail.com>
2019-12-09 17:52:12 -0700

    Reactor javascript and style updates

--- scripts/reactor.js
+++ scripts/reactor.js
@@ -3,12 +3,14 @@ var count = 1;
 $(document).keypress(function(event){ 
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13'){
-  	var currID = "#typewriter-".concat(count);
-  	var curr = document.getElementById(currID);
-  	console.log(currID);
+  	count++;
 
-  	curr.classList.add("typewriter-before");
-  	$("#typewriter-".concat(count + 1)).classList.remove("typewriter-before");
-  	$("#typewriter-".concat(count + 1)).classList.add("typewriter");
+  	var currID = "#typewriter-".concat(count.toString());
+  	var curr = $(currID);
+
+  	
+  	curr.removeClass("typewriter-before");
+  	curr.addClass("typewriter");
   }
 })
+
