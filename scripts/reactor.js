e9676c06db95b6f3ea9d4909fcbf0fe194a9f995
Marissa Angell <maa.angell@gmail.com>
2019-12-10 00:39:30 -0700

    minigame progression

--- scripts/reactor.js
+++ scripts/reactor.js
@@ -14,3 +14,55 @@ $(document).keypress(function(event){
   }
 })
 
+// SET POWER
+
+$('#power-input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+    	var input = $("#power-input").val();
+        if (power_validate(input)){
+        	$("#power-output").val(input);
+        }
+    }
+
+});
+
+function power_validate(input){
+	if (input.match(/[a-zA-Z]$/) || input == ""){
+		event.stopPropagation();
+		alert("Invalid input. Try again.");
+		return false;
+	} else {
+		$("#power-input").attr("readonly", "readonly");
+		return true;
+	}
+
+}
+
+// SET XENON
+
+$('#xenon-input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+    	var input = $("#xenon-input").val();
+        if (xenon_validate(input)){
+        	if (input.toLowerCase() == "y"){
+        		$("#xenon-input").val("Xenon injection activated.");
+        	} else if (input.toLowerCase() == "n"){
+        		
+        	}
+        }
+    }
+
+});
+
+function xenon_validate(input){
+	if (!(input.toLowerCase()).match("y") && !(input.toLowerCase()).match("n")){
+		event.stopPropagation();
+		alert("Invalid input. Try again.");
+		return false;
+	} else {
+		$("#power-input").attr("readonly", "readonly");
+		return true;
+	}
+}
